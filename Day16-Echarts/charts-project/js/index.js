62710ba5318b9bc545aa48426a336e3954f59b71
zuoliang <193957056@qq.com>
2022-02-06 11:08:06 +0800

    2.6

--- Day16-Echarts/charts-project/js/index.js
+++ Day16-Echarts/charts-project/js/index.js
@@ -67,25 +67,79 @@
     // 2.指定配置项和数据
     var option = {
         tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'shadow'
-            }
+            trigger: 'item',
+            // axisPointer: {            // 坐标轴指示器，坐标轴触发有效  这个模块我们此时不需要删掉即可
+            // type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            // }
         },
+        //修改线性渐变色方式1
+        // color: new echarts.graphic.LinearGradient(
+        //     // (x1,y2) 点到点 (x2,y2) 之间进行渐变
+        //     0, 0, 0, 1, [
+        //         { offset: 0, color: '#00fffb' }, // 0 起始颜色
+        //         { offset: 1, color: '#0061ce' } // 1 结束颜色
+        //     ]
+        // ),
+        // 修改线性渐变色方式2
+        color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [{
+                offset: 0,
+                color: '#00fffb' // 0% 处的颜色
+            }, {
+                offset: 1,
+                color: '#0061ce' // 100% 处的颜色
+            }],
+            globalCoord: false // 缺省为 false
+        },
+        // 直角坐标系内绘图网格（区域）
+
         grid: {
-            left: '3%',
-            right: '4%',
+            top: '3%',
+            left: '0%',
+            right: '3%',
             bottom: '3%',
-            containLabel: true
+            //  图表位置紧贴画布边缘是否显示刻度以及label文字 防止坐标轴标签溢出跟grid 区域有关系
+            containLabel: true,
+            show: true,
+            //grid 四条边框的颜色
+            borderColor: 'rgba(0, 240, 255, 0.3)'
         },
+        // 控制x轴
         xAxis: [{
+            // 使用类目，必须有data属性
             type: 'category',
+            // 使用 data 中的数据设为刻度文字
             data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            // 刻度设置
             axisTick: {
-                alignWithLabel: true
-            }
+                // true意思：图形和刻度居中中间
+                // false意思：图形在刻度之间
+
+                alignWithLabel: false,
+                // 不显示刻度
+                show: false,
+            },
+            // x坐标轴文字标签样式设置
+            axisLabel: {
+                color: '#4c9bfd'
+            },
+            // x坐标轴颜色设置
+            axisLine: {
+                lineStyle: {
+                    color: 'rgba(0, 240, 255, 0.3)',
+                    // width:8,  x轴线的粗细
+                    // opcity: 0,   如果不想显示x轴线 则改为 0
+                }
+            },
         }],
+        // 控制y轴
         yAxis: [{
+            // 使用类目，必须有data属性
             type: 'value'
         }],
         series: [{
