d435c67534ae813b9f0c369846c7de46b39e17cb
zuoliang <193957056@qq.com>
2022-02-08 00:31:21 +0800

    2.8

--- Day16-Echarts/charts-project/js/index.js
+++ Day16-Echarts/charts-project/js/index.js
@@ -133,7 +133,7 @@
             type: 'category',
             // 使用 data 中的数据设为刻度文字
             // series
-            data: ["上海", "广州", "北京", "深圳", "合肥", "", "……", "杭州", "厦门", "济南", "成都", "重庆"],
+            data: ["上海", "广州", "北京", "深圳", "合肥", "", "……", "", "厦门", "济南", "成都", "重庆"],
             // 刻度设置
             axisTick: {
                 // true意思：图形和刻度居中中间
@@ -178,4 +178,129 @@
     };
     // 3.把配置给实例对象
     myChart.setOption(option);
-})()
+    window.addEventListener("resize", function() {
+        myChart.resize();
+    })
+})();
+// 订单功能
+// (function() {
+//     // 1. 准备数据
+//     var data = {
+//             day365: { orders: '20,301,987', amount: '99834' },
+//             day90: { orders: '301,987', amount: '9834' },
+//             day30: { orders: '1,987', amount: '3834' },
+//             day1: { orders: '987', amount: '834' }
+//         }
+//         // 获取显示 订单数量 容器
+//     var $h4Orders = $('.order h4:eq(0)')
+//         // 获取显示 金额数量 容器
+//     var $h4Amount = $('.order h4:eq(1)')
+//     $('.order').on('click', '.filter a', function() {
+//             // 2. 点击切换激活样式
+//             $(this).addClass('active').siblings().removeClass('active')
+//                 // 3. 点击切换数据
+//             var currdata = data[this.dataset.key]
+//             $h4Orders.html(currdata.orders)
+//             $h4Amount.html(currdata.amount)
+//         })
+//         // 4. 开启定时器切换数据
+//     var index = 0
+//     var $allTab = $('.order .filter a')
+//     setInterval(function() {
+//         index++
+//         if (index >= 4) index = 0
+//         $allTab.eq(index).click()
+//     }, 5000)
+// })();
+// 销售统计模块
+(function() {
+    var data = {
+            year: [
+                [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+                [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79]
+            ],
+            quarter: [
+                [23, 75, 12, 97, 21, 67, 98, 21, 43, 64, 76, 38],
+                [43, 31, 65, 23, 78, 21, 82, 64, 43, 60, 19, 34]
+            ],
+            month: [
+                [34, 87, 32, 76, 98, 12, 32, 87, 39, 36, 29, 36],
+                [56, 43, 98, 21, 56, 87, 43, 12, 43, 54, 12, 98]
+            ],
+            week: [
+                [43, 73, 62, 54, 91, 54, 84, 43, 86, 43, 54, 53],
+                [32, 54, 34, 87, 32, 45, 62, 68, 93, 54, 54, 24]
+            ]
+        }
+        // 1. 实例化对象
+    var myChart = echarts.init(document.querySelector(".line"));
+    // 2. 指定配置和数据
+    var option = {
+        tooltip: {
+            trigger: "axis"
+        },
+        legend: {
+            data: ["邮件营销", "联盟广告"]
+        },
+        grid: {
+            top: '20%',
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            show: true, // 显示边框
+            borderColor: '#012f4a', // 边框颜色
+            containLabel: true // 包含刻度文字在内
+        },
+        color: ['#00f2f1', '#ed3f35'],
+        xAxis: {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            axisTick: {
+                show: false // 去除刻度线
+            },
+            axisLabel: {
+                color: '#4c9bfd' // 文本颜色
+            },
+            axisLine: {
+                show: false // 去除轴线
+            },
+            boundaryGap: false // 去除轴内间距
+        },
+        yAxis: {
+            type: 'value',
+            axisTick: {
+                show: false // 去除刻度
+            },
+            axisLabel: {
+                color: '#4c9bfd' // 文字颜色
+            },
+            splitLine: {
+                lineStyle: {
+                    color: '#012f4a' // 分割线颜色
+                }
+            }
+        },
+        // 图例组件
+        legend: {
+            textStyle: {
+                color: '#4c9bfd' // 图例文字颜色
+            },
+            right: '10%' // 距离右边10%
+        },
+        series: [{
+            name: '预期销售额',
+            data: [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+            type: 'line',
+            // 折线修饰为圆滑
+            smooth: true,
+        }, {
+            name: '实际销售额',
+            data: [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79],
+            type: 'line',
+            smooth: true,
+        }]
+    };
+
+    // 3. 把配置和数据给实例对象
+    myChart.setOption(option);
+})();
